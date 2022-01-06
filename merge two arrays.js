06859cade21c9e4ab4f150dbe73a232be4fe25ba
Kishore B <kishoreprema2001@gmail.com>
2022-01-06 12:18:55 +0530

    ans

--- merge two arrays.js
+++ merge two arrays.js
@@ -1,5 +1,5 @@
-const array1 = [1,2,9,3,5,1,4,5];
-const array2 = [11,54,70,40];
+var array1 = [1,2,9,3,5,1,4,5];
+var array2 = [11,54,70,40];
 
-const mergedarray = array1.concat(array2); 
+var mergedarray = array1.concat(array2); 
 console.log(mergedarray);
